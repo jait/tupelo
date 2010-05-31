b58641d300acc222d0b30ad6582d496c8b236cfa
Jari Tenhunen <jari.tenhunen@iki.fi>
2010-05-31 21:34:27 +0300

    make server port configurable

--- tupelo-server.py
+++ tupelo-server.py
@@ -2,22 +2,25 @@
 # vim: set sts=4 sw=4 et:
 
 from SimpleXMLRPCServer import SimpleXMLRPCServer
+from optparse import OptionParser
 import xmlrpc
 import logging
-
-#PORT = 8052
-PORT = xmlrpc.DEFAULT_PORT
 
 def _runserver():
     """
     Run, Forrest! Run!
     """
+    parser = OptionParser()
+    parser.add_option("-p", "--port", dest='port', action="store",
+            type="int", default=xmlrpc.DEFAULT_PORT,
+            help="port number for the server")
+    (opts, args) = parser.parse_args()
     format = "server: %(message)s"
     logging.basicConfig(level=logging.INFO, format=format)
-    server = SimpleXMLRPCServer(('localhost', PORT))
+    server = SimpleXMLRPCServer(('localhost', opts.port))
     rpciface = xmlrpc.TupeloXMLRPCInterface()
     server.register_instance(rpciface)
-    logging.info('Tupelo server serving at port %d' % PORT)
+    logging.info('Tupelo server serving at port %d' % opts.port)
     try:
         server.serve_forever()
     except KeyboardInterrupt:
