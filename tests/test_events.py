2a3b4561b3838a15704622a2608ee606b8443cfe
Jari Tenhunen <jari.tenhunen@iki.fi>
2010-11-28 12:32:11 +0200

    more tests, fix indent

--- tests/test_events.py
+++ tests/test_events.py
@@ -7,34 +7,41 @@ import rpc
 
 class TestEventsRPC(unittest.TestCase):
 
-   def testEvent(self, cls=None):
-       cls = events.Event
-       e = cls()
-       e2 = rpc.rpc_decode(events.Event, rpc.rpc_encode(e))
-       self.assert_(isinstance(e2, cls))
-       self.assertEqual(e.type, e2.type)
+    def testEvent(self, cls=None):
+        cls = events.Event
+        e = cls()
+        e2 = rpc.rpc_decode(events.Event, rpc.rpc_encode(e))
+        self.assert_(isinstance(e2, cls))
+        self.assertEqual(e.type, e2.type)
 
-   def testCardPlayedEmpty(self):
-       cls = events.CardPlayedEvent
-       e = cls()
-       e2 = rpc.rpc_decode(events.Event, rpc.rpc_encode(e))
-       self.assert_(isinstance(e2, cls))
-       self.assertEqual(e.type, e2.type)
+    def testCardPlayedEmpty(self):
+        cls = events.CardPlayedEvent
+        e = cls()
+        e2 = rpc.rpc_decode(events.Event, rpc.rpc_encode(e))
+        self.assert_(isinstance(e2, cls))
+        self.assertEqual(e.type, e2.type)
 
-   def testMessageEmpty(self):
-       cls = events.MessageEvent
-       e = cls()
-       e2 = rpc.rpc_decode(events.Event, rpc.rpc_encode(e))
-       self.assert_(isinstance(e2, cls))
-       self.assertEqual(e.type, e2.type)
+    def testMessageEmpty(self):
+        cls = events.MessageEvent
+        e = cls()
+        e2 = rpc.rpc_decode(events.Event, rpc.rpc_encode(e))
+        self.assert_(isinstance(e2, cls))
+        self.assertEqual(e.type, e2.type)
 
-   def testMessage(self):
-       e = events.MessageEvent('from', 'lorem ipsum')
-       e2 = rpc.rpc_decode(events.Event, rpc.rpc_encode(e))
-       self.assert_(isinstance(e2, events.MessageEvent))
-       self.assertEqual(e.type, e2.type)
-       self.assertEqual(e.sender, e2.sender)
-       self.assertEqual(e.message, e2.message)
+    def testMessage(self):
+        e = events.MessageEvent('from', 'lorem ipsum')
+        e2 = rpc.rpc_decode(events.Event, rpc.rpc_encode(e))
+        self.assert_(isinstance(e2, events.MessageEvent))
+        self.assertEqual(e.type, e2.type)
+        self.assertEqual(e.sender, e2.sender)
+        self.assertEqual(e.message, e2.message)
+
+    def testEventListEmpty(self):
+        el = events.EventList()
+        self.assertEqual(len(el), 0)
+        el2 = rpc.rpc_decode(events.EventList, rpc.rpc_encode(el))
+        self.assert_(isinstance(el2, events.EventList))
+        self.assertEqual(len(el2), 0)
 
 
 if __name__ == '__main__':
