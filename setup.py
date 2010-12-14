3ba1cddedb07d428f24a9eaf3dc3e3c8cb56d602
Jari Tenhunen <jari.tenhunen@iki.fi>
2010-12-14 14:56:24 +0200

    remove qcommon module from setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,6 @@ setup (name = "tupelo",
     author_email = "jari.tenhunen@iki.fi",
     license = "BSD",
     packages = ['tupelo'],
-    py_modules = ['qcommon'],
     scripts = ['qtupelo.py', 'tupelo.py', 'tupelo-server.py'],
     platforms="Python 2.4 and later.",
     test_suite = "nose.collector"
