b7eb4f509d712ca1d9648ea931efb8b21c335ba7
Jari Tenhunen <jari.tenhunen@iki.fi>
2011-07-29 12:43:02 +0300

    add StateChangedEvent

--- tests/test_events.py
+++ tests/test_events.py
@@ -50,6 +50,17 @@ class TestEventsRPC(unittest.TestCase):
         self.assertEqual(e.sender, e2.sender)
         self.assertEqual(e.message, e2.message)
 
+    def testStateChanged(self):
+        cls = events.StateChangedEvent
+        game_state = common.GameState()
+        game_state.state = common.GameState.VOTING
+        e = cls(game_state)
+        self.assert_(e.game_state is not None)
+        e2 = rpc.rpc_decode(events.Event, rpc.rpc_encode(e))
+        self.assert_(isinstance(e2, cls))
+        self.assertEqual(e.type, e2.type)
+        self.assertEqual(e.game_state, e2.game_state)
+
     def testEventListEmpty(self):
         el = events.EventList()
         self.assertEqual(len(el), 0)
